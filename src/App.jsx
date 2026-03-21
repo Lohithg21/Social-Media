8545f48f86d136ced903348cbe113d0ec8547415
Lohith G <lohithashwa944@gmail.com>
2026-03-21 15:35:43 +0530

    created header in feature1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div>Placeholder</div>;
+  return(
+  <>
+    <div>Placeholder</div>;
+    <Header></Header>
+  </>
+  );
 }
 
 export default App;
