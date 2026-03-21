cb7eba72c62009041074747a0fff73d7c651a8fa
Lohith G <lohithashwa944@gmail.com>
2026-03-21 15:41:47 +0530

    created footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return(
   <>
-    <div>Placeholder</div>;
     <Header></Header>
+    <Footer></Footer>
   </>
   );
 }
