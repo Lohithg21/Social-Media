0611cd520778dfdc97568cf59fb024fc6e063126
Lohith G <lohithashwa944@gmail.com>
2026-03-23 11:40:14 +0530

    created Create Post UI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,7 @@
-const Sidebar = () => {
+
+const Sidebar = ({selectedTab , setSelectedTab}) => {
+
+
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -18,9 +21,9 @@ const Sidebar = () => {
       <hr />{" "}
       <ul className="nav nav-pills flex-column mb-auto">
         {" "}
-        <li className="nav-item">
+        <li className="nav-item" onClick ={() => setSelectedTab("Home")}>
           {" "}
-          <a href="#" className="nav-link active" aria-current="page">
+          <a href="#" className={`nav-link text-white ${selectedTab === "Home" ? "active" : ""}`} aria-current="page">
             {" "}
             <svg
               className="bi pe-none me-2"
@@ -33,9 +36,9 @@ const Sidebar = () => {
             Home
           </a>{" "}
         </li>{" "}
-        <li>
+        <li onClick={() => {setSelectedTab("CreatePost")}}>
           {" "}
-          <a href="#" className="nav-link text-white">
+          <a href="#" className={`nav-link text-white ${selectedTab === "CreatePost" ? "active" : ""}`}>
             {" "}
             <svg
               className="bi pe-none me-2"
---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,52 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext(
+  {
+    postList: [],
+    addPost: () => {},
+    deletePost: () => {},
+  }
+);
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter((post) => post.id !== action.payload.postId);
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
+
+  const addPost = () => {};
+
+  const deletePost = (postId) => {
+    dispatchPostList({ type: "DELETE_POST", payload: { postId } });
+  };
+
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [{
+  id: '1',
+  title: 'Going to Mumbai',
+  body: 'Hi friends, I am going to Mumbai for my vacations, Hope to enjoy a lot. Peace out.',
+  reactions: 2,
+  userId: 'user-9',
+  tags: ['vacations', 'india', 'mumbai'],
+},{
+  id: '2',
+  title: 'Pass ho bhai',
+  body: '4 saal ki masti ke baad bhi ho gaye hain pass. Hard to believe',
+  reactions: 15,
+  userId: 'user-12',
+  tags: ['graduating','unbelievable'],
+}];
+
+export default PostListProvider;
